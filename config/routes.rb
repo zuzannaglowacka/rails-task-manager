8c7c95d552774c0db4d96d250efd56576dc05ca3
Zuzanna Glowacka <zuzglowacka@gmail.com>
2017-11-08 10:49:29 +0000

    Pushing everything from yesterday

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
+  get "tasks/:id", to: "tasks#show", as: "task"
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update", as: "task_edit"
+  delete "tasks/:id", to: "tasks#destroy"
 end
